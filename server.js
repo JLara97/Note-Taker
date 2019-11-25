b6198b54ec6eaae89d60eb5f6d6d2976a9f74f9e
Jacob Lara <l1a9r9a7@gmail.com>
2019-11-24 19:24:01 -0700

    Added other routes

--- server.js
+++ server.js
@@ -23,7 +23,56 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/api/notes", async function (req, res) {
 
+    try {
+        const notes = await readFile("./db/db.json", "utf8");
+        res.json(JSON.parse(notes));
+    } catch (err) {
+        res.status(500).end();
+    }
+
+});
+
+app.post("/api/notes", async function (req, res) {
+    try {
+        const notes = JSON.parse(
+            await readFile("./db/db.json", "utf8")
+        );
+        const note = { ...req.body, id: uuid() };
+        console.log(note);
+        notes.push(note);
+        await writeFile("./db/db.json", JSON.stringify(notes, null, 2));
+
+        res.json(note);
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+
+
+app.delete("/api/notes/:id", async function (req, res) {
+
+    try {
+        const id = req.params.id;
+
+        const notes = JSON.parse(
+            await readFile("./db/db.json", "utf8")
+        );
+
+        let note = notes.filter(note => note.id != id);
+
+        await writeFile("./db/db.json", JSON.stringify(note, null, 2));
+
+        res.json(note)
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
